b1b83d2f20421376f0d57756c84f936350f5a70b
imadityaxd <104629524+imadityaxd@users.noreply.github.com>
2024-12-22 19:58:10 +0530

    backend

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 auth = Blueprint('auth', __name__)
 
@@ -16,13 +19,13 @@ def logout():
 def sign_up():
     if request.method =='POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
         if len(email) < 4:
             flash('Email must be greater than 3 characters.', category='error')
-        elif len(firstName)< 3:
+        elif len(first_name)< 3:
             flash('First Name must be greater than 2 characters.', category='error')
         elif password1 !=password2:
             flash('Password and confirm password should be same.', category='error')
@@ -30,5 +33,10 @@ def sign_up():
             flash('Password must be greater than 6 characters.', category='error')
         else:
             #add user to database
+            new_user = User(email=email, first_name=first_name,password=generate_password_hash(password1, method='pbkdf2:sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account created successfully.', category='success')
+            return redirect(url_for('views.home'))
+
     return render_template("sign_up.html")
